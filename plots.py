fixed bug with wrong shapes

plots.py
@@ -9,11 +9,10 @@ def oscilator(t,x,omega=4, ksi=2):
 def test_rce(t,x):
     return x + np.sin(t)
 
-reseni = euler(oscilator,np.array([1,1]), 0, 10, 0.01, implicit=True)
+reseni = euler(oscilator,np.array([1,1]), 0, 10, 0.01, implicit=False)
 x,t = reseni.as_arrays()
 
 fig, ax = plt.subplots()
 ax.plot(x)
 plt.show()
-
-print(len(reseni(np.linspace(0,11,100))))
+print("len(reseni.t):", len(reseni.t))

results.py
@@ -66,7 +66,7 @@ class Result:
         dx_arr = np.asarray(self.dx)
         #najít index intervalu, indexy jsou pole, pro numpy to lze dělat vektorově
         i = search(t_arr, t_eval)
-        i = clip(i, 0, len(self.t)-3)
+        i = clip(i, 0, len(t_arr)-2)
         #normování
         h = t_arr[i+1] - t_arr[i]
         s = (t_eval - t_arr[i]) / h